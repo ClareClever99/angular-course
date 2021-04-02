1a62ae33614e7793f4f21c8e67e0696517672b18
Clare Clever <59885439+Robert-Clever@users.noreply.github.com>
2021-04-02 10:06:14 -0400

    Added new code...

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { EventEmitter } from 'events';
 import { Course } from '../model/course';
 
 @Component({
@@ -11,9 +12,19 @@ export class CourseCardComponent implements OnInit {
   @Input()
   course:Course;
 
+  @Output()
+  courseSelected = new EventEmitter<Course>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onCourseViewed() {
+    console.log("card component - button clicked ...");
+
+    this.courseSelected.emit(this.course);
+
   }
 
 }

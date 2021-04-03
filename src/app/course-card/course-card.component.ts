e93f16975fbdb730c6af08bae6f9c28134e3d756
Clare Clever <59885439+Robert-Clever@users.noreply.github.com>
2021-04-03 19:36:48 -0400

    New code...

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -43,7 +43,9 @@ export class CourseCardComponent implements OnInit {
   }
 
   cardStyles() {
-    return {'text-decoration': 'underline'}
+    return {
+      'background-image': 'url(' + this.course.iconUrl + ')'
+    };
   }
 
 }

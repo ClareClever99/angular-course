99472f2635ab0cb0bfecbaf163b27faab23a188b
Clare Clever <59885439+Robert-Clever@users.noreply.github.com>
2021-04-04 08:41:10 -0400

    Added new code...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,8 @@ export class AppComponent {
 
   courses = COURSES;
 
+  startDate = new Date(2000, 0, 1);
+
   onCourseSelected(course: Course) {
     console.log('App component - click event bubbled...', course);
   }
